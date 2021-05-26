461e3f5af42d5bd4ce3db4fdcd87238e38ea2cca
unnati gupta <unnatigupta.2320@gmail.com>
2021-05-26 21:32:39 +0530

    last changes

--- codes/ASSIGNMENT2.py
+++ codes/ASSIGNMENT2.py
@@ -5,8 +5,28 @@ import matplotlib.pyplot as plt
 #Quadrilateral vertices
 M = np.array([0,0])
 I = np.array([3.5,0])
-S=np.array([5.46,6.036])
-T=np.array([2.42,9.63])
+S=np.array([5.18,6.27])
+T=np.array([2.42,9.03])
+
+def print_coordinates_of_triangle_MIS(m,i,s):
+    """m, i and s are lengths of the sides of a triangle MIS"""
+
+    M= (0, 0) # coordinates of vertex M
+
+    I= (s, 0) # coordinates of vertex I
+    
+
+    S_x = i * Decimal(i**2 + s**2 - m**2) / (2 * i * s)
+    S_y = Decimal(i**2 - S_x**2).sqrt() # square root
+
+    S = (float(S_x), float(S_y)) # coordinates of vertex S
+
+    # print
+    vertices = zip(["M = ", "I= ", "S = "], [M, I, S])
+    print('\n'.join([v[0] + str(v[1]) for v in vertices]))
+
+    # test -- print coordinates of triangle given sides of length 6.5, 8.14, 3.5
+    print_coordinates_of_triangle_MIS(6.5,8.14,3.5)
 
 #Generating all lines
 x_MI = line_gen(M,I)
