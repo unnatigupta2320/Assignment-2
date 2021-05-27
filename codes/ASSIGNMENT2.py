d9c7b2227e684b4bc4f6a610fb0b5bf870175b98
unnati gupta <unnatigupta.2320@gmail.com>
2021-05-27 10:56:36 +0530

    Changes in Code

--- codes/ASSIGNMENT2.py
+++ codes/ASSIGNMENT2.py
@@ -2,31 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+  #Triangle vertices
+def myMacro() :
+ def myVec_tri_vert_MIS(m,i,s):
+  p = (s**2 + i**2-m**2 )/(2*s)
+  q = np.sqrt(i**2-p**2)
+  S = np.array([p,q]) 
+  M = np.array([0,0]) 
+  I = np.array([s,0]) 
+  return  M,I,S
+
+  #Triangle sides
+s = 3.5
+m = 6.5
+i = 8.14
+
+  #Coordinates of T
+k = (s**2 + i**2-m**2 )/(2*s)
+l = np.sqrt(i**2-k**2)
+print(k,l)
+
 #Quadrilateral vertices
 M = np.array([0,0])
 I = np.array([3.5,0])
 S=np.array([5.18,6.27])
 T=np.array([2.42,9.03])
-
-def print_coordinates_of_triangle_MIS(m,i,s):
-    """m, i and s are lengths of the sides of a triangle MIS"""
-
-    M= (0, 0) # coordinates of vertex M
-
-    I= (s, 0) # coordinates of vertex I
-    
-
-    S_x = i * Decimal(i**2 + s**2 - m**2) / (2 * i * s)
-    S_y = Decimal(i**2 - S_x**2).sqrt() # square root
-
-    S = (float(S_x), float(S_y)) # coordinates of vertex S
-
-    # print
-    vertices = zip(["M = ", "I= ", "S = "], [M, I, S])
-    print('\n'.join([v[0] + str(v[1]) for v in vertices]))
-
-    # test -- print coordinates of triangle given sides of length 6.5, 8.14, 3.5
-    print_coordinates_of_triangle_MIS(6.5,8.14,3.5)
 
 #Generating all lines
 x_MI = line_gen(M,I)
